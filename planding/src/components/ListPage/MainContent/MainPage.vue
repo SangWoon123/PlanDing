f034f5f539e0b2a1b67f9b3decdd24acd085c845
SangWoon123 <alrax3574@naver.com>
2024-07-31 16:13:56 +0900

    feat: 메인페이지 백그라운드 디자인

---
+++ planding/src/components/ListPage/MainContent/MainPage.vue
@@ -0,0 +1,57 @@
+<template>
+  <section id="container">
+    <div class="left"></div>
+    <div class="mid">
+      <div class="inner"></div>
+    </div>
+    <div class="right"></div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+#container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f5f5fd;
+
+  .left {
+    background-color: #ffffff;
+    width: 88px;
+    height: 781px;
+    border-radius: 16px;
+    box-shadow:
+      0 4px 6px -1px rgb(0 0 0 / 0.1),
+      0 2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  .mid {
+    background-color: #ffffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1012px;
+    height: 848px;
+    border-radius: 16px;
+    margin: 0 15px 0 15px;
+    box-shadow:
+      0 4px 6px -1px rgb(0 0 0 / 0.1),
+      0 2px 4px -2px rgb(0 0 0 / 0.1);
+    .inner {
+      background-color: #e8e9fa;
+      width: 972px;
+      height: 808px;
+      border-radius: 4px;
+    }
+  }
+  .right {
+    background-color: #ffffff;
+    width: 320px;
+    height: 860px;
+    border-radius: 16px;
+  }
+}
+</style>
--- planding/src/router.js
+++ planding/src/router.js
@@ -1,12 +1,14 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import LoginPage from './components/LoginComponent/LoginPage.vue'
 import LoginProgress from './components/LoginComponent/LoginProgress.vue'
+import MainPage from './components/ListPage/MainContent/MainPage.vue'
 
 const router = new createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: LoginPage },
-    { path: '/LoginProgressPage', component: LoginProgress }
+    { path: '/LoginProgressPage', component: LoginProgress },
+    { path: '/planding', component: MainPage }
   ]
 })
 
