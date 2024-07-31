c43362b056b521b5f66eec0af7f4984d56e5f99c
SangWoon123 <alrax3574@naver.com>
2024-07-31 18:56:47 +0900

    feat: Pretendard font추가

--- planding/src/App.vue
+++ planding/src/App.vue
@@ -3,3 +3,13 @@
 </template>
 
 <script setup></script>
+
+<style>
+@font-face {
+  font-family: 'Pretendard-Regular';
+  src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+    format('woff');
+  font-weight: 400,500,700,800,900;
+  font-style: normal;
+}
+</style>
