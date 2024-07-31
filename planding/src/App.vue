190b58cc54275314754d324a277e3b281d7a7fe0
SangWoon123 <alrax3574@naver.com>
2024-07-31 15:46:08 +0900

    build: vuetify

--- planding/src/App.vue
+++ planding/src/App.vue
@@ -3,9 +3,3 @@
 </template>
 
 <script setup></script>
-
-<style lang="scss" scoped>
-h2{
-  color:rebeccapurple
-}
-</style>
--- planding/src/main.js
+++ planding/src/main.js
@@ -1,9 +1,16 @@
-import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 import router from './router'
 import { createPinia } from 'pinia'
 
+loadFonts()
+
 const pinia = createPinia()
 
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App)
+  .use(vuetify)
+  .use(router)
+  .use(pinia)
+  .mount('#app')
