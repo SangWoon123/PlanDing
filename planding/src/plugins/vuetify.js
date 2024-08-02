2943525411249dd235f47c2746058b7bf4d6899e
SangWoon123 <alrax3574@naver.com>
2024-08-02 13:09:46 +0900

    build:vuetify 플러그인

--- planding/src/plugins/vuetify.js
+++ planding/src/plugins/vuetify.js
@@ -4,11 +4,11 @@ import 'vuetify/styles'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { VCalendar } from 'vuetify/labs/VCalendar'
 
-export default createVuetify(
+export default createVuetify({
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-  components,
-  directives
-)
+  components: {
+    VCalendar
+  }
+})
