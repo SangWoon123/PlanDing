72f7ec3852c220a3fe6569b1cd44e84f1a0ae499
SangWoon123 <alrax3574@naver.com>
2024-08-02 13:29:28 +0900

    build: vuetify 언어 플러그인 설정

--- planding/src/plugins/vuetify.js
+++ planding/src/plugins/vuetify.js
@@ -5,9 +5,14 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 import { VCalendar } from 'vuetify/labs/VCalendar'
+import { ko } from 'vuetify/locale'
 
 export default createVuetify({
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+  locale: {
+    locale: 'ko',
+    messages: { ko }
+  },
   components: {
     VCalendar
   }
