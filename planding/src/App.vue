0a9b808c09aa08db8e9f90338a00672bec2288db
SangWoon123 <alrax3574@naver.com>
2024-08-13 19:18:04 +0900

    fix: App.vue

--- planding/src/App.vue
+++ planding/src/App.vue
@@ -1,45 +1,6 @@
 <template>
-  <div>
-    <GlobalAlert :data="data" @remove="removeAlert" />
-    <router-view />
-  </div>
+  <router-view />
 </template>
-
-<script setup>
-import { onMounted, onUnmounted, ref } from 'vue'
-import { sseConnect } from './service/sseService'
-import GlobalAlert from './components/SmallTools/GlobalAlert.vue'
-
-const connect = ref(null)
-const data = ref([])
-onMounted(() => {
-  connect.value = sseConnect()
-
-  connect.value.onopen = () => {
-    console.log('Connection to server opened.')
-  }
-
-  connect.value.onmessage = function (e) {
-    const parsedData = JSON.parse(e.data)
-    data.value = [...data.value, parsedData] // 불변성을 유지하며 데이터 추가
-    console.log(data.value)
-  }
-
-  connect.value.onerror = (e) => {
-    console.log('sse 에러', e)
-    connect.value.close()
-    connect.value = sseConnect()
-  }
-})
-
-function removeAlert(item) {
-  data.value = data.value.filter((alert) => alert.id !== item.scheduleId)
-}
-
-onUnmounted(() => {
-  connect.value.close()
-})
-</script>
 
 <style>
 @font-face {
