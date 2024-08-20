618684fee734b1459b9b73883120f4b6850caf85
SangWoon123 <alrax3574@naver.com>
2024-08-20 22:00:33 +0900

    feat: ADD버튼 컴포넌트 추가

---
+++ planding/src/components/ui/AddButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="button">
+    <div class="content">
+      <slot name="icon"></slot>
+      <slot name="text"></slot>
+    </div>
+  </button>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.button {
+  width: 113px;
+  height: 40px;
+  background-color: #474aa1;
+  border-radius: 8px;
+  .content {
+    display: flex;
+    justify-content: center;
+    font-size: 15px;
+    gap:2px;
+  }
+}
+</style>
