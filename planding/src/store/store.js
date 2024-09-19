81471245b13017dd6d77bba5e319dbfb82c629e7
SangWoon123 <alrax3574@naver.com>
2024-09-19 21:00:55 +0900

    fix: 리프레스 토큰

--- planding/src/store/store.js
+++ planding/src/store/store.js
@@ -1,28 +1,35 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
-import router from '@/router'
 import { authInstance } from '@/service/authAxios'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     accessToken: localStorage.getItem('accessToken') || '',
+    refreshToken: localStorage.getItem('refreshToken') || '',
     userName: '',
     userCode: '',
     profileImage: ''
   }),
   actions: {
-    saveToken(token) {
+    saveAccessToken(token) {
       this.accessToken = token
       localStorage.setItem('accessToken', token)
-      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+      // axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    },
+    saverefreshToken(token) {
+      this.refreshToken = token
+      localStorage.setItem('refreshToken', token)
+      // axios.defaults.headers.common['refreshToken'] = `Bearer ${token}`
     },
     clearAuth() {
       this.accessToken = ''
+      this.refreshToken = ''
       this.userName = ''
       this.userCode = ''
       localStorage.removeItem('accessToken')
+      localStorage.removeItem('refreshToken')
       delete axios.defaults.headers.common['Authorization']
-      router.push('/')
+      // router.push('/')
     },
     async getUserInfo() {
       const response = await authInstance('/api/v1/profile').get()
